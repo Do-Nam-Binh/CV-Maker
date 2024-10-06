7282e7773e1db101b788c6053b83e4f745f2ae62
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2024-10-06 10:56:13 +0700

    Add resume preview component

---
+++ src/components/ResumePreview.jsx
@@ -0,0 +1,33 @@
+import styles from "../styles/ResumePreview.module.css"
+
+function ResumePreview({fullName, headline, personalDetails}) {
+    return(
+        <>
+        <div className={styles.body}>
+            <div className={styles.leftSide}>
+                <div className={styles.nameHeadline}>
+                    <div className={styles.name}>
+                        <div>{fullName.firstName}</div>
+                        <div>{fullName.lastName}</div>  
+                    </div>
+
+                    <div className={styles.headline}>Healine example for preview</div>
+                </div>
+                
+                <div className={styles.leftHeaders}>Personal details</div>
+                <div className={styles.leftHeaders}>Skills</div>
+                <div className={styles.leftHeaders}>Languages</div>
+                <div className={styles.leftHeaders}>Hobbies</div>
+            </div>
+
+            <div className={styles.rightSide}>
+                <div className={styles.rightHeaders}>Education</div>
+                <div className={styles.rightHeaders}>Employment</div>
+                <div className={styles.rightHeaders}>Projects</div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default ResumePreview
